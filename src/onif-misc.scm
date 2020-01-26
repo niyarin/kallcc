5d5ce47b3621ae7694478c728bdb3b510a92853e
niyarin <inoanet@gmail.com>
2020-01-26 21:07:36 +0900

    Add support for built-in-function.

--- src/onif-misc.scm
+++ src/onif-misc.scm
@@ -1,10 +1,12 @@
 (include "./onif-symbol.scm")
 
-(define-library (onif misc)   
+(define-library (onif misc)
    (import (scheme base)
            (srfi 125)
            (onif symbol))
-   (export onif-misc/make-check-onif-symbol-base-function 
+   (export onif-misc/make-check-onif-symbol-base-function
+           onif-misc/lambda-operator?
+           onif-misc/lambda-meta-operator?
            onif-misc/onif-symbol-hash-ref)
    (begin
      (define (onif-misc/make-check-onif-symbol-base-function target-symbol)
@@ -15,7 +17,14 @@
                    operator))
              (else #f))))
 
+     (define onif-misc/lambda-meta-operator?
+       (onif-misc/make-check-onif-symbol-base-function 'lambda-META))
+
+     (define onif-misc/lambda-operator?
+       (onif-misc/make-check-onif-symbol-base-function 'lambda))
+
      (define (onif-misc/onif-symbol-hash-ref onif-symbol-hash symbol)
         (cond
           ((hash-table-ref onif-symbol-hash symbol) => cadr)
-          (else #f)))))
+          (else #f)))
+     ))
