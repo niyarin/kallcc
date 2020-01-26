21ed4cb6b78554b5c8e367f6df4753bb8b04b235
niyarin <inoanet@gmail.com>
2020-01-26 19:36:54 +0900

    Add idebug option for formatting meta-lambda informations.

--- src/onif-idebug.scm
+++ src/onif-idebug.scm
@@ -10,6 +10,50 @@
          => cadr)
         (else #f)))
 
+
+     (define (%icode->code/meta-lambda-conv icode opt)
+      (let ((not-contain-meta-info
+              (%opt-ref opt 'not-contain-meta-info))
+            (shaping-meta-info
+              (%opt-ref opt 'shaping-meta-info)))
+        (cond
+          (not-contain-meta-info
+           (%icode->code/expression-conv
+                 (cons 'lambda
+                   (cons (cadr icode) (cdddr icode)))
+                 opt))
+          (shaping-meta-info
+            (let loop ((infos (caddr icode))
+                       (res '()))
+              (if (null? infos)
+                `(LAMBDA-META
+                   ,(map
+                      (lambda (x)
+                        (%icode->code x opt))
+                      (cadr icode))
+                  ,@(append
+                     '({)
+                     (reverse res)
+                      '(}))
+                  .
+                  ,(map
+                     (lambda (x)
+                         (%icode->code x opt))
+                     (cdddr icode)))
+                (let ((key (caar infos))
+                      (val (cadar infos)))
+                  (loop
+                    (cdr infos)
+                    (cons
+                      ","
+                      (cons
+                         (%icode->code val opt)
+                         (cons
+                            (string-append (symbol->string key) ":")
+                            res))))))))
+          (else
+           (map (lambda (x) (%icode->code x opt)) icode)))))
+
      (define (%icode->code/expression-conv icode opt)
        (let ((operator
                (if (onif-symbol/onif-symbol? (car icode))
@@ -17,12 +61,7 @@
                  (car icode))))
           (case operator
             ((LAMBDA-META)
-             (if (%opt-ref opt 'not-contain-meta-info)
-               (%icode->code/expression-conv
-                 (cons 'lambda
-                   (cons (cadr icode) (cdddr icode)))
-                 opt)
-              (map (lambda (x) (%icode->code x opt)) icode)))
+             (%icode->code/meta-lambda-conv icode opt))
             (else
               (map (lambda (x) (%icode->code x opt)) icode)))))
 
