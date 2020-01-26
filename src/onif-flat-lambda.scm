a14518334395f38da5cd27263e93bac35441bde9
niyarin <inoanet@gmail.com>
2020-01-26 21:06:34 +0900

    Add aux data for closure conversion.

--- src/onif-flat-lambda.scm
+++ src/onif-flat-lambda.scm
@@ -1,12 +1,15 @@
 (include "./onif-misc.scm")
+(include "./onif-idebug.scm")
 
 (define-library (onif flat-lambda)
    (import (scheme base)
            (scheme cxr)
+           (onif idebug)
+           (only (scheme list) filter)
            (srfi 125);scheme hash
            (onif misc)
            (onif symbol))
-   (export onif-flat-flat-code&id-lambdas) 
+   (export onif-flat-flat-code&id-lambdas)
    (begin
      (define %lambda-operator?
        (onif-misc/make-check-onif-symbol-base-function 'lambda))
@@ -17,28 +20,45 @@
      (define %if-operator?
        (onif-misc/make-check-onif-symbol-base-function 'if))
 
-     (define (%flat-conv 
-               code 
-               lambdas-box 
-              offset-box 
-              symbol-hash 
-              expand-environment)
-       (cond 
-         ((not (pair? code))
-          code)
+     (define (%list-set-difference2 ls1 ls2)
+       (filter
+         (lambda (x) (not (member x ls2)))
+         ls1))
+
+     (define (%flat-conv
+               code
+               lambdas-box
+               prev-info
+               offset-box
+               symbol-hash
+               expand-environment)
+       (cond
+         ((not (pair? code)) code)
          ((%lambda-meta-operator? (car code) symbol-hash);<= ONLY body is 1.
             (let* ((id (car offset-box))
                    (_ (set-car!  offset-box (+ id 1)))
-                   (new-body (%flat-conv 
+                   (new-body (%flat-conv
                                (cadddr code)
                                lambdas-box
+                               (caddr code)
                                offset-box
                                symbol-hash
                                expand-environment))
+                   (prev-contain-symbols
+                     (cond
+                       ((assq 'contain-symbols prev-info)
+                        => cadr)
+                       (else '())))
+                   (contain-symbols
+                     (%list-set-difference2
+                        (%list-set-difference2
+                           prev-contain-symbols
+                           (cadr (assq 'contain-symbols (caddr code))))
+                        (cadr code)))
                    (new-lambda (list (car code);OPE
-                              (cadr code);FORMALS
-                              (caddr code);META INFO
-                              new-body)))
+                                     (cadr code);FORMALS
+                                     (caddr code);META INFO
+                                     new-body)))
                (begin
                   (set-car!
                     lambdas-box
@@ -47,29 +67,33 @@
                       (car lambdas-box)))
                   (list
                     (onif-misc/onif-symbol-hash-ref symbol-hash 'LFUN)
-                    id))))
-         (else 
-           (map 
+                    id
+                    contain-symbols))))
+         (else
+           (map
              (lambda (x)
-               (%flat-conv 
+               (%flat-conv
                  x
                  lambdas-box
+                 prev-info
                  offset-box
                  symbol-hash
                  expand-environment))
              code))))
 
-     (define (onif-flat-flat-code&id-lambdas 
+     (define (onif-flat-flat-code&id-lambdas
                code
-               lambda-id-offset 
-               syntax-symbol-hash  
+               lambda-id-offset
+               syntax-symbol-hash
                expand-environment)
        (let* ((lambdas-box (list '()))
-              (flat-code (%flat-conv 
-                           code 
+              (prev-info '())
+              (flat-code (%flat-conv
+                           code
                            lambdas-box
-                           (list lambda-id-offset) 
-                           syntax-symbol-hash 
+                           prev-info
+                           (list lambda-id-offset)
+                           syntax-symbol-hash
                            expand-environment)))
          (values
            flat-code
