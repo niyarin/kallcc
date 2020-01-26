df231353aa45326c2b590276fa74dc2ff25b48a8
niyarin <inoanet@gmail.com>
2020-01-26 21:07:05 +0900

    Fix coding style.

--- src/onif-meta-lambda.scm
+++ src/onif-meta-lambda.scm
@@ -6,7 +6,7 @@
    (import (scheme base)
            (scheme list);SRFI 1
            (scheme cxr)
-           (onif idebug)
+           (onif idebug) (scheme write)
            (onif misc)
            (onif symbol))
    (export onif-meta-lambda-conv
@@ -25,16 +25,16 @@
        (onif-misc/make-check-onif-symbol-base-function 'lambda-META))
 
      (define (%make-meta-info . options)
-         (let ((stack 
+         (let ((stack
                  (cond ((assq 'stack options) => cadr)(else '())))
-               (base 
+               (base
                  (cond ((assq 'base options) => cadr)(else #f)))
-               (formals 
+               (formals
                  (cond ((assq 'formals options) => cadr )(else '())))
                (contain-symbols
                  (cond ((assq 'contain-symbols options) => cadr )(else '()))))
-            (list 
-              (list 'bind-names 
+            (list
+              (list 'bind-names
                     (append (apply append stack)
                             formals))
               (list 'contain-symbols contain-symbols))))
@@ -43,7 +43,7 @@
         (list
          (car meta-lambda-code)
          (cadr meta-lambda-code)
-         (cons 
+         (cons
            (list key val)
            (caddr meta-lambda-code))
          (cadddr meta-lambda-code)))
@@ -65,40 +65,40 @@
          new-body))
 
      (define (%conv-meta-lambda cps-code onif-symbol-hash stk onif-expand-env)
-       (cond 
+       (cond
          ((not (pair? cps-code)) cps-code)
          ((%lambda-operator? (car cps-code) onif-symbol-hash)
           (let* ((new-stk (cons (cadr cps-code) stk))
                  (new-body
-                   (map 
+                   (map
                       (lambda (body)
-                        (%conv-meta-lambda 
+                        (%conv-meta-lambda
                           body
                           onif-symbol-hash
                           new-stk
                           onif-expand-env))
                       (cddr cps-code)))
                  (symbol-list
-                   (map 
+                   (map
                      (lambda (x)
                        (if (and (pair? x)
                                 (%lambda-meta-operator? (car x) onif-symbol-hash))
                          (cadr (assq 'contain-symbols (caddr x)))
                          '()))
-                     (apply append 
+                     (apply append
                             (filter list? new-body))))
                  (contain-symbols
                    (apply append (cons (cadr cps-code) symbol-list))))
             `(,(onif-misc/onif-symbol-hash-ref onif-symbol-hash 'lambda-META)
              ,(cadr cps-code)
-             ,(%make-meta-info 
+             ,(%make-meta-info
                `(stack ,stk)
                `(formals ,(cadr cps-code))
                `(contain-symbols  ,contain-symbols))
              .
              ,new-body)))
-         (else 
-           (map 
+         (else
+           (map
               (lambda (x)
                 (%conv-meta-lambda x onif-symbol-hash stk onif-expand-env))
               cps-code))))
