ff18a0e113c7b38f57c3376ad0097a66493c029d
niyarin <inoanet@gmail.com>
2020-01-28 19:38:37 +0900

    Change onfi-name-ids to generating name-ids each meta-lambdas.

--- src/onif-name-ids.scm
+++ src/onif-name-ids.scm
@@ -8,49 +8,31 @@
            (onif meta-lambda))
    (export name-ids/make-name-local-ids)
    (begin
-     (define (%make-name-local-ids code not-first-lambda offset onif-symbol-hash)
-       (cond
+     (define (%make-name-local-ids code bind-vars onif-symbol-hash)
+      (cond
          ((not (pair? code)) code)
-         ((and not-first-lambda
-               (onif-misc/lambda-meta-operator? (car code) onif-symbol-hash))
-          (onif-meta-lambda/update-meta-info-body
-            code
-            'local-ids not-first-lambda
-            (%make-name-local-ids
-              (cadddr code)
-              not-first-lambda
-              offset
-              onif-symbol-hash)))
          ((onif-misc/lambda-meta-operator? (car code) onif-symbol-hash)
-          "META LAMBA MUST ALLOWS ONLY 1 ARGUMENT."
-          (let* ((contain-symbols
-                  (cadr (assq 'contain-symbols (caddr code))))
-                 (local-ids
-                   (do ((id offset (+ id 1))
-                        (_contain-symbols
-                          contain-symbols
-                          (cdr _contain-symbols))
-                        (res '() (cons (list (car _contain-symbols) id) res)))
-                       ((null? _contain-symbols) res)))
+          (let* ((bind-vars (append (cadr code) bind-vars))
+                 (name-ids
+                   (do ((vars bind-vars (cdr vars))
+                        (res '() (cons (list (car vars) id) res))
+                        (id 0 (+ id 1)))
+                     ((null? vars) res)))
                  (new-body
                    (%make-name-local-ids
                      (cadddr code)
-                     local-ids
-                     offset
+                     bind-vars
                      onif-symbol-hash)))
-              (onif-meta-lambda/update-meta-info-body
-                code
-                'local-ids local-ids
-                new-body)))
+                (onif-meta-lambda/update-meta-info-body
+                  code
+                  'local-ids
+                  name-ids
+                  new-body)))
          (else
            (map
              (lambda (x)
-               (%make-name-local-ids
-                 x
-                 not-first-lambda
-                 offset
-                 onif-symbol-hash))
+                 (%make-name-local-ids x bind-vars onif-symbol-hash))
              code))))
 
      (define (name-ids/make-name-local-ids code onif-symbol-hash)
-       (%make-name-local-ids code #f 0 onif-symbol-hash))))
+      (%make-name-local-ids code '()  onif-symbol-hash))))
