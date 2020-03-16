91c3a73d3da8d1ec5aa54291af72df8e6430f98d
niyarin <inoanet@gmail.com>
2020-03-17 02:23:23 +0900

    Fix some problems.

--- src/onif-like-asm.scm
+++ src/onif-like-asm.scm
@@ -11,14 +11,19 @@
            (onif misc))
    (export
      onif-like-asm/convert
-     onif-like-asm/convert-function)
+     onif-like-asm/convert-functions
+     onif-like-asm/make-global-ids-box)
+
    (begin
+     (define (onif-like-asm/make-global-ids-box)
+       (list '() 0))
+
      (define (%atom-conv register code)
          (cond
            ((char? code)
             `(SET! (R ,register) code))
            (else
-              (error "TBA"))))
+              (error "TBA 3"))))
 
      (define %lfun?
        (onif-misc/make-check-onif-symbol-base-function 'LFUN))
@@ -35,40 +40,68 @@
           (cond
             ((assq var local-ids)
              => (lambda (x)
-                   `(SET! ,offset ,(+ org-offset x))))
+                   `(SET! ,offset ,(+ org-offset x))));<= To change Register ???
             (else
               `(SET! (R ,offset) (GLOBAL ,var))))))
 
-     (define (%expand-arg exps offset last-meta-info onif-symbol-hash)
+     (define (%expand-arg exps offset last-meta-info global-ids-box onif-symbol-hash)
          (let loop ((code exps)
                     (offset offset)
                     (rev-res '()))
+           (display "CODE:")(display code)(newline)
            (cond
              ((null? code) (reverse rev-res))
-             ((or (onif-symbol? (car code)) (symbol? (car code)))
+             ((or (onif-symbol? (car code)) (symbol? (car code)));symbol
+              (let* ((var-info;(SET! target from)
+                      (%var-ref
+                        (car code)
+                        (%meta-info-ref last-meta-info 'local-ids)
+                        offset
+                        (%meta-info-ref last-meta-info 'local-register-offset)))
+                     (var-position (caddr var-info))
+                     (var-code
+                       (cond
+                         ((not (and (pair? var-position)
+                                    (eq? (car var-position) 'GLOBAL)))
+                          var-info)
+                         ((assq (cadr var-position) (car global-ids-box))
+                          => (lambda (id)
+                               `(SET! ,(cadr var-info) (G ,(cadr var-position)))));TODO:FIX
+                         ((cadr global-ids-box)
+                          =>
+                          (lambda (id)
+                              (set-car!
+                                global-ids-box
+                                (cons
+                                  (list (cadr var-position) (+ id 1))
+                                  (car global-ids-box)))
+                              (set-cdr!
+                                global-ids-box
+                                (+ 1 id))
+                              `(SET! ,(cadr var-info) (G ,id))))
+                         (else
+                           (error "Unexpected variable" var-position)))))
+              (display "???")(display (list var-code))(newline)
+              ;VAR-CODE IS <UNSPECIFIED>. FIX IT.
               (loop
                 (cdr code)
                 (+ offset 1)
                 (cons
-                  (%var-ref
-                    (car code)
-                    (%meta-info-ref last-meta-info 'local-ids)
-                    offset
-                    (%meta-info-ref last-meta-info 'local-register-offset))
-                  rev-res)))
-             ((not (pair? code))
+                  var-code
+                  rev-res))))
+             ((not (pair? (car code)));atom
               (loop
                 (cdr code)
                 (+ offset 1)
                 (cons (%atom-conv offset code) rev-res)))
-             ((%lfun? (caar code) onif-symbol-hash)
-              (let* ((make-env-vector
-                       `(MAKE-VECTOR ,(length (caddar code)) (R ,offset)))
+             ((%lfun? (caar code) onif-symbol-hash);LOAD FUN
+              (let* ((make-env-vector `(MAKE-VECTOR (R ,offset)
+                                                    ,(length (caddar code))))
                      (local-ids (%meta-info-ref last-meta-info 'local-ids))
                      (registers
                        (map
                          (lambda (x)
-                           (+ offset 1 (cadr (assq  local-ids x))))
+                           (+ offset 1 (cadr (assq  x local-ids))))
                          (caddar code)))
                      (set-env-vectors;順序は気にしなくて良い
                        (map
@@ -101,6 +134,7 @@
                code
                register-offset
                last-meta-info
+               global-ids-box
                onif-symbol-hash)
        (cond
          ((not (pair? code))
@@ -112,6 +146,7 @@
                           (cdr code)
                           register-offset
                           last-meta-info
+                          global-ids-box
                           onif-symbol-hash))
                        (operation-code
                          (%operation-conv operator register-offset)))
@@ -120,23 +155,41 @@
            (cond
              (else
                (display (vector "?" (onif-idebug-icode->code code)))(newline)
-               (error "TBA"))))))
+               (error "TBA! 1"))))))
 
-     (define (onif-like-asm/convert-function meta-functions offset onif-symbol-hash)
+     (define (onif-like-asm/convert-functions
+               meta-functions
+               offset
+               global-ids-box
+               onif-symbol-hash)
       (let loop ((functions meta-functions)
-                  (offset offset)
-                  (res '()))
+                 (offset offset)
+                 (res '()))
          (if (null? functions)
-           '()
-           (let* ((function (cadar functions))
-                  (bindings (cadr function))
-                  (meta-info (caddr function))
+           res
+           (let* ((meta-function (cadar functions))
+                  (bindings (cadr meta-function))
+                  (meta-info (caddr meta-function))
                   (local-ids
                     (%meta-info-ref meta-info 'local-ids))
-                  (body (cadddr function))
+                  (body (cadddr meta-function))
                   (function-address offset))
-               (let* ()
-                 (newline)
-                 (display local-ids)(newline)
-                 (newline)
-                 (loop (cdr functions) offset res))))))))
+                 (display "BODY::")(display (onif-idebug-icode->code  body '(not-contain-meta-info #t)))(newline)
+
+               (display "META-INFO=")(display meta-info)(newline)
+               (let* ((binding-list '());TBA
+                      (register-position
+                         (map
+                           (lambda (apair)
+                             (list
+                               (car apair)
+                               (+ (cadr apair) offset)))
+                           local-ids))
+                      (_res (append
+                              res
+                        (onif-like-asm/convert body
+                                               offset
+                                               meta-info
+                                               global-ids-box
+                                               onif-symbol-hash))))
+                 (loop (cdr functions) offset _res))))))))
