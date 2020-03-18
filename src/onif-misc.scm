cb04f3d6d085f91d9bf1503c8a30923aeb200381
niyarin <inoanet@gmail.com>
2020-03-18 23:11:43 +0900

    Add some aux functions.

--- src/onif-misc.scm
+++ src/onif-misc.scm
@@ -3,13 +3,18 @@
 (define-library (onif misc)
    (import (scheme base)
            (srfi 125)
-           (scheme write);
+           (scheme cxr)
            (onif symbol))
    (export onif-misc/make-check-onif-symbol-base-function
            onif-misc/lambda-operator?
+           onif-misc/begin-operator?
            onif-misc/lambda-meta-operator?
            onif-misc/onif-symbol-hash-ref
-           onif-misc/ref-operations)
+           onif-misc/ref-operations
+           onif-misc/filter-&-elses
+           onif-misc/tail-pair
+           onif-misc/tail-pair-res
+           onif-misc/tail-pair-push!)
    (begin
      (define (onif-misc/make-check-onif-symbol-base-function target-symbol)
        (lambda (operator onif-symbol-hash)
@@ -25,6 +30,9 @@
      (define onif-misc/lambda-operator?
        (onif-misc/make-check-onif-symbol-base-function 'lambda))
 
+     (define onif-misc/begin-operator?
+       (onif-misc/make-check-onif-symbol-base-function 'begin))
+
      (define (onif-misc/onif-symbol-hash-ref onif-symbol-hash symbol)
         (cond
           ((hash-table-ref onif-symbol-hash symbol) => cadr)
@@ -36,5 +44,28 @@
          (case (onif-symbol/ref-symbol operator)
             ((CONS CAR CDR PAIR?)
              => (lambda (x) x))
-            (else
-              #f))))))
+            (else #f))))
+
+     (define (onif-misc/tail-pair)
+        (let ((head (cons #f #f)))
+          (cons head head)))
+
+     (define onif-misc/tail-pair-res cadar)
+
+     (define (onif-misc/tail-pair-push! tail-pair x)
+       (set-cdr! (cdr tail-pair) (list x))
+       (set-cdr! tail-pair (cddr tail-pair)))
+
+     (define (onif-misc/filter-&-elses fn ls)
+       (let ((res1 (onif-misc/tail-pair))
+             (elses (onif-misc/tail-pair)))
+          (let loop ((ls ls))
+            (cond ((null? ls) (values
+                                (onif-misc/tail-pair-res res1)
+                                (onif-misc/tail-pair-res elses)))
+                  ((fn (car ls))
+                   (onif-misc/tail-pair-push! res1 (car ls))
+                   (loop (cdr ls)))
+                  (else
+                   (onif-misc/tail-pair-push! elses (car ls))
+                (loop (cdr ls)))))))))
