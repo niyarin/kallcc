97fed18b474f6cfba1a31c906c7c3e8c388043c7
niyarin <inoanet@gmail.com>
2020-03-27 04:46:40 +0900

    Add aux library.

---
+++ src/lib/thread-syntax.scm
@@ -0,0 +1,11 @@
+(define-library (niyarin thread-syntax)
+   (import (only (scheme base) define-syntax syntax-rules begin ...))
+   (export ->>)
+   (begin
+      (define-syntax ->>
+         (syntax-rules ()
+           ((_ x) x)
+           ((_ x (op args ...) rest ...)
+            (->> (op args ... x) rest ...))
+           ((_ x op rest ...)
+            (->> (op x) rest ...))))))
