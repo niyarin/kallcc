94b37035863eb7e7700342ce7d5757d2b1fd1ba0
niyarin <inoanet@gmail.com>
2020-11-07 13:17:31 +0900

    Add scmspec

---
+++ src/lib/scmspec.scm
@@ -0,0 +1,36 @@
+(define-library (scmspec core)
+   (import (scheme base))
+   (export scmspec/lcheck)
+   (begin
+     (define-syntax %apply-spec
+       (syntax-rules ()
+          ((%apply-spec input spec spec-mode)
+             (let ((spec-res (spec input)))
+               (if spec-res
+                 #t
+              (error "scm spec error." spec-mode (quote spec) (quote input)))))))
+
+     (define-syntax %input-specs
+        (syntax-rules ()
+           ((_ ()) #t)
+           ((_ ((sym spec) rest ...))
+            (begin (%apply-spec sym spec "input spec")
+                   (%input-specs (rest ...))))))
+
+     (define-syntax %lcheck-aux
+         (syntax-rules (input output)
+            ((_ () #f bodies ...) (begin bodies ...))
+            ((_ () res bodies ...) res)
+            ((_ ((input input-spec) rest ...) res bodies ...)
+             (begin
+               (%input-specs input-spec)
+               (%lcheck-aux (rest ...) res bodies ...)))
+            ((_ ((output output-spec) rest ...) res bodies ...)
+             (let ((<result> (begin bodies ...)))
+               (%apply-spec <result> output-spec "output spec")
+               (%lcheck-aux (rest ...) <result>)))))
+
+     (define-syntax scmspec/lcheck
+        (syntax-rules ()
+         ((_ specs bodies ...)
+          (%lcheck-aux specs #f bodies ...))))))
