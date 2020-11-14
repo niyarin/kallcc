0e80df871346ebb9453c13b747235891244acc39
niyarin <inoanet@gmail.com>
2020-11-14 18:24:16 +0900

    Fix indent.

--- src/onif-symbol.scm
+++ src/onif-symbol.scm
@@ -1,9 +1,8 @@
-
 (define-library (onif symbol)
    (import (scheme base)
            (scheme case-lambda))
-
-   (export onif-symbol onif-symbol? onif-symbol->symbol onif-symbol/onif-symbol?  onif-symbol/ref-symbol)
+   (export onif-symbol onif-symbol? onif-symbol->symbol
+           onif-symbol/onif-symbol?  onif-symbol/ref-symbol)
    (begin
      (define *id-counter* 0)
      (define-record-type <onif-symbol>
@@ -12,20 +11,17 @@
          (base-symbol %onif-symbol-ref-base-symbol)
          (id %onif-symbol-ref-id))
 
-     (define onif-symbol/onif-symbol?
-        onif-symbol?)
+     (define onif-symbol/onif-symbol? onif-symbol?)
 
-     (define onif-symbol/ref-symbol
-       %onif-symbol-ref-base-symbol)
+     (define onif-symbol/ref-symbol %onif-symbol-ref-base-symbol)
 
      (define onif-symbol
-         (case-lambda
-           (()
-            (onif-symbol '_))
-           ((base-symbol)
-            (let ((id *id-counter*))
-              (set! *id-counter* (+ *id-counter* 1))
-              (%onif-symbol base-symbol *id-counter*)))))
+       (case-lambda
+         (() (onif-symbol '_))
+         ((base-symbol)
+          (let ((id *id-counter*))
+            (set! *id-counter* (+ *id-counter* 1))
+            (%onif-symbol base-symbol *id-counter*)))))
 
       (define (onif-symbol->symbol osym);naive
         (string-append
