14755991efc33f0ea3e693e1c26e899a113b5ed6
niyarin <inoanet@gmail.com>
2020-12-16 11:33:18 +0000

    Add kallcc symbol.

---
+++ src/kallcc-symbol.scm
@@ -0,0 +1,8 @@
+(define-library (kallcc symbol)
+   (import (scheme base)
+           (onif symbol))
+   (export kallcc-symbol kallcc-symbol?)
+   (begin
+     (define kallcc-symbol onif-symbol)
+     (define kallcc-symbol? onif-symbol?)
+   ))
