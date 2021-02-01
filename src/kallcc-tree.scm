9243fd7f3807545efccc0961f25fcdcb6338f83c
niyarin <inoanet@gmail.com>
2021-02-02 01:35:53 +0900

    Add update-tree procedure.

--- src/kallcc-tree.scm
+++ src/kallcc-tree.scm
@@ -1,13 +1,27 @@
 (define-library (kallcc tree)
-  (import (scheme base) (scheme list) (scheme vector))
-  (export any)
+  (import (scheme base) (scheme list) (scheme vector) (scheme write))
+  (export (rename any* any)
+          (rename update* update))
   (begin
-    (define (any tree pred . accessors)
+    ;;TODO:線形更新も追加したい
+    (define (any* pred tree . accessors)
       ;;unsupport circular object
       ;;accessors... TBW
-      (let loop ((tree tree) (res '()))
+      (let loop ((tree tree))
         (cond
-          ((list? tree) (any pred tree))
+          ((list? tree)
+           (or (any pred tree)
+               (any loop tree)))
           ((vector? tree) (vector-any pred tree))
-          (else #f))))))
+          ;;pair?
+          (else #f))))
 
+    (define (update* pred update-proc tree . accessors)
+      (let loop ((tree tree))
+        (cond
+          ((list? tree)
+           (map (lambda (x)
+                 (let ((y (loop x)))
+                   (if (pred y) (update-proc y) y)))
+               tree))
+          (else tree))))))
--- test/test-tree.scm
+++ test/test-tree.scm
@@ -2,13 +2,21 @@
 
 (import (scheme base)
         (srfi 64)
+        (scheme write)
         (prefix (kallcc tree) ktree/))
 
-(begin;;tconc-test
+(begin;;any-test
    (test-begin "test-any")
    (test-assert (ktree/any integer? '(1 2 3)))
    (test-assert (ktree/any integer? '((1 2 3))))
    (test-assert (ktree/any integer? '(hello (1 2 3))))
+   (test-assert (ktree/any integer? '(hello ((1 2 3)))))
+   (test-assert (not (ktree/any integer? '(a b c))))
    (test-end "test-any"))
 
+(begin;;update-test
+   (test-begin "test-update")
+   (test-equal (ktree/update integer? (lambda (x) (+ x 1)) '(1 2 3)) '(2 3 4))
+   (test-equal (ktree/update integer? (lambda (x) (+ x 1)) '((1) (2) (3) (hello))) '((2) (3) (4) (hello)))
+   (test-end "test-update"))
 
